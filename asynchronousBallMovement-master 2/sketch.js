37467ac2ec8952a9d95303e27baf12061215adfa
vidaasbe <66427463+vidaasbe@users.noreply.github.com>
2020-08-13 15:52:06 +0530

    Add files via upload

---
+++ asynchronousBallMovement-master 2/sketch.js
@@ -0,0 +1,25 @@
+var database;
+var gameState = 0;
+var playerCount;
+var player,form,game;
+var allPlayers;
+var car1,car2,car3,car4;
+var cars=[];
+
+function setup(){
+    database = firebase.database();
+    createCanvas(displayWidth-20,displayHeight-30);
+    game = new Game();
+    game.getState();
+    game.start();
+}
+
+function draw(){
+ if(playerCount === 4){
+     game.update(1);
+ }
+ if(gameState === 1){
+     clear();
+     game.play();
+ }
+}
